df6353e9dac43ca4682c388d49626a6acbd9ee7b
drakazz <drakazz@50bc374b-9529-0410-a273-830fcf80c12f>
2007-08-11 16:05:33 +0000

    Updated auto-detection of where the system lives in. No need for configuration.

--- sys/config.inc.php
+++ sys/config.inc.php
@@ -5,10 +5,20 @@ function config()
 	if (!isset($co) )
 	{
 		$co = new stdClass();
-$co->pdo = 'sqlite:xmain.db';
-$co->path_absolute = '/pastebox';
-$co->path_global = 'http://'.$_SERVER['HTTP_HOST'].$co->path_absolute;
-$co->numentries = 10;
+		$co->pdo = 'sqlite:xmain.db';
+		$parts = explode('/',$_SERVER['SCRIPT_NAME']);
+		$n = array_search('sys', $parts);
+		$pp = array_slice($parts, 0, $n);
+		$co->path_absolute = implode("/",$pp);
+		if (substr($co->path_absolute, 0, 1) == '/' )
+		{
+			$co->path_global = 'http://'.$_SERVER['HTTP_HOST'].$co->path_absolute;
+		}
+		else
+		{
+			$co->path_global = 'http://'.$_SERVER['HTTP_HOST'].'/'.$co->path_absolute;
+		}
+		$co->numentries = 10;
 	}
 	return $co;
 }
