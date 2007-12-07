473b1e1857382f15750a908a18e73c3d3cd4a9d2
drakazz <drakazz@50bc374b-9529-0410-a273-830fcf80c12f>
2007-12-07 08:29:26 +0000

    MySQL added

--- sys/setup.php
+++ sys/setup.php
@@ -4,8 +4,16 @@ $d = db_get();
 try { $st = $d->query('SELECT id FROM entry LIMIT 1;'); $st->execute();echo"No need for new tables, seems to work."; }
 catch(Exception $e)
 {
+	if ($db->getAttribute(PDO::ATTR_DRIVER_NAME) == 'sqlite')
+	{
 	//$d->query('CREATE TABLE comment (id INTEGER PRIMARY KEY, content TEXT, date INT, entry INT, space TEXT, ip TEXT);');
-	$d->query('CREATE TABLE entry (id INTEGER PRIMARY KEY, type TEXT, subject TEXT, description TEXT, content TEXT, ip TEXT, name TEXT, comments INT, date INT, space TEXT);');
-	echo "<p>Tables were created.</p>";
+		$d->query('CREATE TABLE entry (id INTEGER PRIMARY KEY, type TEXT, subject TEXT, description TEXT, content TEXT, ip TEXT, name TEXT, comments INT, date INT, space TEXT);');
+		echo "<p>Tables were created.</p>";
+	}
+	else
+	{
+		$d->query('CREATE TABLE entry (id INTEGER PRIMARY KEY AUTO_INCREMENT, type TEXT, subject TEXT, description TEXT, content TEXT, ip TEXT, name TEXT, comments INT, date INT, space TEXT);');
+		echo "<p>Tables were created.</p>";
+	}
 }
 
